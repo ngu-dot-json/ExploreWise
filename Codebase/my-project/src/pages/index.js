9d81e484e3275fddb78fbb3e5a5683250c79ed5a
Jason Ngu <jason.ngu058@gmail.com>
2023-11-15 00:44:36 -0700

    setup attempt to add background image to project

--- Codebase/my-project/src/pages/index.js
+++ Codebase/my-project/src/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Navbar from '../../components/Navbar'
 import Schedule from '../../components/TLScheduler'
+import Image from 'next/image'
+import background from "../public/test.png"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +21,7 @@ export default function Home() {
       <h1>MAP VIEW</h1>
     </div>
 
+
     <heading className='Naver'>
       WELCOME TO THE HOME PAGE<br/>
       WELCOME TO THE HOME PAGE<br/>
@@ -28,6 +31,17 @@ export default function Home() {
       WELCOME TO THE HOME PAGE<br/>
       WELCOME TO THE HOME PAGE<br/>
     </heading>
+
+
+    <main className="flex flex-col items-center justify-between">
+      <div className='relative w-full'>
+        <div className='absolute -z-10 w-full'>
+          <Image src={background} alt="background image" className="w-full" width={1000}/>
+        </div>
+        <div></div>
+      </div>
+    </main>
+
 
     </>
   )
