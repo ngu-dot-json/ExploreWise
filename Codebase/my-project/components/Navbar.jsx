e29aeabfd3d2c6573ca38099a70d5beb14865d21
Jason Ngu <jason.ngu058@gmail.com>
2023-11-15 15:05:25 -0700

    adjustment of styling and introduction of a home landing page

--- Codebase/my-project/components/Navbar.jsx
+++ Codebase/my-project/components/Navbar.jsx
@@ -12,17 +12,16 @@ const Navbar = () => {
         setMenuOpen(!menuOpen);
     }
 
-    return (
-    <nav className="fixed w-full h-15 shadow-xl bg-white">
+    return ( // bg-white shadow-xl
+    <nav className="fixed w-full h-15">
         <div className="flex justify-between items-center h-full w-full px-4">
             
             {/* Pressable Logo */}
             <Link href='/'>
-                <Image
+                <Image className="logo"
                 src={Logo}
                 alt="logo"
                 height="50"
-                className="cursor-pointer"
                 priority
                 />
             </Link>
@@ -31,23 +30,23 @@ const Navbar = () => {
             <div classname>
             {/* <div classname="hidden sm:flex"> */}
                 <ul className="hidden sm:flex">
-                <Link href="/mapview">
+                    <Link className='navElements' href="/mapview">
                         <li className="ml-10 mr-10 hover:border-b text-l">Itinerary</li>
                     </Link>
 
-                    <Link href="/mapview">
+                    <Link className='navElements' href="/mapview">
                         <li className="ml-10 mr-10 hover:border-b text-l">Map View</li>
                     </Link>
 
-                    <Link href="/findevents">
+                    <Link className='navElements' href="/findevents">
                         <li className="ml-10 mr-10 hover:border-b text-l">Find Events</li>
                     </Link>
 
-                    <Link href="/budgettracker">
+                    <Link className='navElements' href="/budgettracker">
                         <li className="ml-10 mr-10 hover:border-b text-l">Budget Tracker</li>
                     </Link>
                     
-                    <Link href="/account">
+                    <Link className='navElements' href="/account">
                         <li className="ml-10 mr-10 hover:border-b text-l">Account</li>
                     </Link>
                 </ul>
--- Codebase/my-project/src/pages/index.js
+++ Codebase/my-project/src/pages/index.js
@@ -7,37 +7,26 @@ import background from "/public/test.png"
 
 const inter = Inter({ subsets: ['latin'] })
 
-// const styles = {
-//   heading:{
-//     marginTop: "120px", //Shifting the heading down so it is not covered by the navigation bar
-//   }
-// }
-
 export default function Home() {
   return (
     <>
     <div>
       <Navbar />  
-      <h1>MAP VIEW</h1>
       <br/>
     </div>
 
     {/* background image container */}
     <main className="flex flex-col items-center justify-between">
       <div className='relative w-full'>
-        <div className='absolute -z-10 w-full'>
-          <Image src={background} alt="background image" className="w-full" width={1000}/>
+        <div className='absolute -z-10 mt-[-25px] w-full h-full'>
+          <Image src={background} alt="background image" className="w-full" width={1000} height={1000}/>
         </div>
         <div>
-        <heading className='text-align: center'>
-
-        <h2 className="ewHeading">Welcome To:</h2>
-        <h1 className="ewEW">ExploreWise</h1>
-        <h3 className='ewText'>Your Modern Travel Planning Platform.</h3>
-    </heading>
-
-
-          
+          <heading className='text-align: center'>
+            <h2 className="ewHeading">Welcome To:</h2>
+            <h1 className="ewEW">ExploreWise</h1>
+            <h3 className='ewText'>Your Modern Travel Planning Platform.</h3>
+          </heading> 
         </div>
       </div>
     </main>
