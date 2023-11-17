edfcf67d559f8e377111ae92212a67a0a77d0061
Jason Ngu <jason.ngu058@gmail.com>
2023-11-17 10:28:40 -0700

    edits to attempt a switchover to a new scheduler platform

--- Codebase/my-project/src/pages/old-gantt-v1.js
+++ Codebase/my-project/src/pages/old-gantt-v1.js
@@ -13,90 +13,57 @@ async function initializeGSTC(element) {
 
   // helper functions
 
+  function generateRows() {
+    /**
+     * @type { import("gantt-schedule-timeline-calendar").Rows }
+     */
+    const rows = {};
+    for (let i = 0; i < 100; i++) {
+      const id = GSTC.api.GSTCID(i.toString());
+      rows[id] = {
+        id,
+        label: `Row ${i}`,
+      };
+    }
+    return rows;
+  }
+
+  function generateItems() {
+    /**
+     * @type { import("gantt-schedule-timeline-calendar").Items }
+     */
+    const items = {};
+    // @ts-ignore
+    let start = GSTC.api.date().startOf("day").subtract(6, "day");
+    for (let i = 0; i < 100; i++) {
+      const id = GSTC.api.GSTCID(i.toString());
+      const rowId = GSTC.api.GSTCID(Math.floor(Math.random() * 100).toString());
+      start = start.add(1, "day");
+      items[id] = {
+        id,
+        label: `Item ${i}`,
+        rowId,
+        time: {
+          start: start.valueOf(),
+          end: start.add(1, "day").endOf("day").valueOf(),
+        },
+      };
+    }
+    return items;
+  }
   const date = GSTC.api.date;
   /**
    * @type { import("gantt-schedule-timeline-calendar").Config }
    */
-
-  const itemsFromDB = [
-    {
-      id: "1",
-      label: "Item 1",
-      rowId: "1",
-      time: {
-        start: GSTC.api.date("2020-01-01").startOf("day").valueOf(),
-        end: GSTC.api.date("2020-01-02").endOf("day").valueOf(),
-      },
-    },
-    {
-      id: "2",
-      label: "Item 2",
-      rowId: "1",
-      time: {
-        start: GSTC.api.date("2020-02-01").startOf("day").valueOf(),
-        end: GSTC.api.date("2020-02-02").endOf("day").valueOf(),
-      },
-    },
-    {
-      id: "3",
-      label: "Item 3",
-      rowId: "2",
-      time: {
-        start: GSTC.api.date("2020-01-15").startOf("day").valueOf(),
-        end: GSTC.api.date("2020-01-20").endOf("day").valueOf(),
-      },
-    },
-  ];
-  
-  const columnsFromDB = [
-    {
-      id: "id",
-      label: "ID",
-      data: ({ row }) => GSTC.api.sourceID(row.id), // show original id (not internal GSTCID)
-      sortable: ({ row }) => Number(GSTC.api.sourceID(row.id)), // sort by id converted to number
-      width: 80,
-      header: {
-        content: "ID",
-      },
-    },
-    {
-      id: "label",
-      data: "label",
-      sortable: "label",
-      isHTML: false,
-      width: 230,
-      header: {
-        content: "Label",
-      },
-    },
-  ];
-
-
-  const rowsFromDB = [
-    {
-      id: "1",
-      label: "Row 1",
-    },
-    {
-      id: "2",
-      label: "Row 2",
-    },
-  ];
-
-
-
   const config = {
     licenseKey:
       "====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====",
     plugins: [TimelinePointer(), Selection(), ItemResizing(), ItemMovement()],
     list: {
       columns: {},
-      rows: GSTC.api.fromArray(rowsFromDB),      // rows: generateRows(),
+      rows: generateRows(),
     },
     chart: {
-      config: {
-        innerHeight: 900,
-      },
       time: {
         // from: date("2020-01-01").valueOf(), // from 2020-01-01
         // to: date("2020-01-01").endOf("day").valueOf(), // to 2020-01-31
@@ -106,7 +73,8 @@ async function initializeGSTC(element) {
         onDate: [({ date }) => date],
 
       },
-      items: GSTC.api.fromArray(itemsFromDB),    },
+      items: generateItems(),
+    },
   };
 
   state = GSTC.api.stateFromConfig(config);
@@ -159,41 +127,3 @@ export default function Home() {
     </div>
   );
 }
-
-// function generateRows() {
-  //   /**
-  //    * @type { import("gantt-schedule-timeline-calendar").Rows }
-  //    */
-  //   const rows = {};
-  //   for (let i = 0; i < 100; i++) {
-  //     const id = GSTC.api.GSTCID(i.toString());
-  //     rows[id] = {
-  //       id,
-  //       label: `Row ${i}`,
-  //     };
-  //   }
-  //   return rows;
-  // }
-  // function generateItems() {
-  //   /**
-  //    * @type { import("gantt-schedule-timeline-calendar").Items }
-  //    */
-  //   const items = {};
-  //   // @ts-ignore
-  //   let start = GSTC.api.date().startOf("day").subtract(1, "day");
-  //   for (let i = 0; i < 100; i++) {
-  //     const id = GSTC.api.GSTCID(i.toString());
-  //     const rowId = GSTC.api.GSTCID(Math.floor(Math.random() * 100).toString());
-  //     start = start.add(1, "day");
-  //     items[id] = {
-  //       id,
-  //       label: `Item ${i}`,
-  //       rowId,
-  //       time: {
-  //         start: start.valueOf(),
-  //         end: start.add(1, "day").endOf("day").valueOf(),
-  //       },
-  //     };
-  //   }
-  //   return items;
-  // }
---
+++ Codebase/my-project/src/pages/old-iten.js
@@ -0,0 +1,76 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import Navbar from '../../components/Navbar'
+import React from "react";
+const inter = Inter({ subsets: ['latin'] })
+import Map from '../../components/Map';
+import Schedule from '../../components/TLScheduler'
+import Image from 'next/image'
+import backgrounder from "/public/test.png"
+
+
+
+
+function Mapview(){
+  return (
+    <>
+    <div>
+      <Navbar />  
+      <br/>
+    </div>
+
+    {/* background image container */}
+    <main className="flex flex-col items-center justify-between">
+      <div className='relative w-full'>
+        <div className='absolute -z-10 mt-[-25px] w-full h-full'>
+        <Image className="bImg" src={backgrounder} alt="background image" width={1000} height={1000}/>
+        </div>
+        <div>
+          <Map/>
+          <Schedule/>
+        </div>
+      </div>
+    </main>
+    </>
+  );
+}
+
+
+
+// function Mapview() {
+//   return (
+//     <>
+
+//       <div>
+//         <Navbar />  
+//       </div>
+//       <div id="root">
+//         <Map/>
+//         <Schedule/>
+//       </div>
+    
+//     </>
+//   );
+// }
+
+
+
+
+
+// function Mapview() {
+//   return (
+
+//     <div>
+//       <Navbar />  
+//       <h1>MAP VIEW</h1>
+//       <Map
+//     />
+
+//   <div id="root"></div>
+//       <Schedule
+// />
+//     </div>
+//   );
+// }
+
+export default Mapview;
