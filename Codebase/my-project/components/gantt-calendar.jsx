559a5c6b976694c6224a33f24a6a3192e0ea79d6
Jason Ngu <jason.ngu058@gmail.com>
2023-11-17 17:36:38 -0700

    major stylistic overhaul of alternative timeline scheduler

--- Codebase/my-project/components/gantt-calendar.jsx
+++ Codebase/my-project/components/gantt-calendar.jsx
@@ -21,8 +21,9 @@ async function initializeGSTC(element) {
   const itemsFromDB = [
     {
       id: "1",
-      label: "Item 1",
+      label: "Item 1 <br/> Test",
       rowId: "1",
+      height:300,
       time: {
         start: GSTC.api.date("2020-01-01").startOf("day").valueOf(),
         end: GSTC.api.date("2020-01-02").endOf("day").valueOf(),
@@ -33,74 +34,36 @@ async function initializeGSTC(element) {
       label: "Item 2",
       rowId: "1",
       time: {
-        start: GSTC.api.date("2020-02-01").startOf("day").valueOf(),
-        end: GSTC.api.date("2020-02-02").endOf("day").valueOf(),
-      },
-    },
-    {
-      id: "3",
-      label: "Item 3",
-      rowId: "2",
-      time: {
-        start: GSTC.api.date("2020-01-15").startOf("day").valueOf(),
-        end: GSTC.api.date("2020-01-20").endOf("day").valueOf(),
+        start: GSTC.api.date("2020-01-01").startOf("day").valueOf(),
+        end: GSTC.api.date("2020-01-02").endOf("day").valueOf(),
       },
     },
   ];
   
-  const columnsFromDB = [
-    {
-      id: "id",
-      label: "ID",
-      data: ({ row }) => GSTC.api.sourceID(row.id), // show original id (not internal GSTCID)
-      sortable: ({ row }) => Number(GSTC.api.sourceID(row.id)), // sort by id converted to number
-      width: 80,
-      header: {
-        content: "ID",
-      },
-    },
-    {
-      id: "label",
-      data: "label",
-      sortable: "label",
-      isHTML: false,
-      width: 230,
-      header: {
-        content: "Label",
-      },
-    },
-  ];
-
-
   const rowsFromDB = [
     {
       id: "1",
       label: "Row 1",
-    },
-    {
-      id: "2",
-      label: "Row 2",
+      height:400,
     },
   ];
-
-
 
   const config = {
     licenseKey:
       "====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====",
     plugins: [TimelinePointer(), Selection(), ItemResizing(), ItemMovement()],
     list: {
-      columns: {},
+    //   columns: {},
       rows: GSTC.api.fromArray(rowsFromDB),      // rows: generateRows(),
     },
     chart: {
       config: {
-        innerHeight: 900,
+        // innerHeight: 700,
       },
       time: {
         // from: date("2020-01-01").valueOf(), // from 2020-01-01
         // to: date("2020-01-01").endOf("day").valueOf(), // to 2020-01-31
-        zoom: 16,
+        zoom: 16.05,
         onLevelDates: [({ dates }) => dates],
         onCurrentViewLevelDates: [({ dates }) => dates],
         onDate: [({ date }) => date],
@@ -129,14 +92,6 @@ const Gantt = () => {
       }
     };
   });
-
-  function updateFirstRow() {
-    if (!GSTC || !state) return;
-    state.update(`config.list.rows.${GSTC.api.GSTCID("0")}`, (row) => {
-      row.label = "Changed dynamically";
-      return row;
-    });
-  }
 
   return (
     <div className="container">
@@ -201,3 +156,35 @@ export default Gantt;
   //   }
   //   return items;
   // }
+
+//   const columnsFromDB = [
+//     {
+//       id: "id",
+//       label: "ID",
+//       data: ({ row }) => GSTC.api.sourceID(row.id), // show original id (not internal GSTCID)
+//       sortable: ({ row }) => Number(GSTC.api.sourceID(row.id)), // sort by id converted to number
+//       width: 80,
+//       header: {
+//         content: "ID",
+//       },
+//     },
+//     {
+//       id: "label",
+//       data: "label",
+//       sortable: "label",
+//       isHTML: false,
+//       width: 230,
+//       height: 300,
+//       header: {
+//         content: "Label",
+//       },
+//     },
+//   ];
+
+// function updateFirstRow() {
+//     if (!GSTC || !state) return;
+//     state.update(`config.list.rows.${GSTC.api.GSTCID("0")}`, (row) => {
+//       row.label = "Changed dynamically";
+//       return row;
+//     });
+//   }
