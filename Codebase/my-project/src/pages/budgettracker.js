75cf99b095d6f42d21d46326fbb712a57e54c89f
Jason Ngu <jason.ngu058@gmail.com>
2023-11-25 19:59:51 -0700

    stylistic adjustments and structure adjustments to budget-tracker

--- Codebase/my-project/src/pages/budgettracker.js
+++ Codebase/my-project/src/pages/budgettracker.js
@@ -14,11 +14,13 @@ import ExpenseTotal from '../../components/BudgetTrackerComponents/ExpenseTotal'
 import ExpenseList from '../../components/BudgetTrackerComponents/ExpenseList';
 import AddExpenseForm from '../../components/BudgetTrackerComponents/AddExpenseForm';
 import RemainingBudget from '../../components/BudgetTrackerComponents/Remaining';
+import Image from 'next/image'
+import backgrounder from "/public/test.png"
 
 
 const styles = {
   heading:{
-    marginTop: "120px", //Shifting the heading down so it is not covered by the navigation bar
+    marginTop: "60px", //Shifting the heading down so it is not covered by the navigation bar
   }
 }
 
@@ -26,57 +28,63 @@ const App = () => {
   
 	return (
     
+    <>
 		<AppProvider>
 
-      <div className='row mt-3'>
-					<div className='col-sm'>
-          <Navbar />
-					</div>
-			</div>
+      <div>
+        <Navbar />  
+        <br/>
+      </div>
       
-      
-			<div className='container'>
-				<h1 style={styles.heading}>My Budget Planner</h1>
+      {/* background image container */}
+      <main className="flex flex-col items-center justify-between">
+        <div className='relative w-full'>
+          <div className='absolute -z-10 mt-[-25px] w-full h-full'>
+            <Image className="bImg" src={backgrounder} alt="background image" width={1000} height={1000}/>
+          </div>
+          
+          <div className='container'>
+            <h1 style={styles.heading}>My Budget Planner</h1>
+            
+            <div class='container'>
+              <div className='row mt-3'>
+                <div className='col-lg-12'>
+                  <div class="alert alert-info" role="alert">
+                    Set your total budget for the trip, then start adding expenses!
+                  </div>
+                </div>
+              </div>
+              
+              <div className='row mt-2'>
+                <div className='col-lg'>
+                  <div className='row'>
+                    <div className='col-lg-6'>
+                      <Budget />
+                    </div>
+                    
+                    <div className='col-lg-6'>
+                      <RemainingBudget />
+                    </div>
+                  </div>
 
+                  <div className='row'>
+                    <div className='col-lg-12'>
+                      <AddExpenseForm />
+                    </div>
+                  </div>
 
-      <div class='container'>
-        <div className='row mt-3'>
-          <div className='col-lg-12'>
-            <div class="alert alert-info" role="alert">
-              Set your total budget for the trip, then start adding expenses!
+                </div>
+                <div className='col-lg-8'>
+                  <ExpenseList />
+                </div>
+
+              </div>
             </div>
           </div>
         </div>
-
-
-				<div className='row mt-2'>
-
-          <div className='col-lg'>
-            <div className='row'>
-              <div className='col-lg-6'>
-                <Budget />
-              </div>
-              <div className='col-lg-6'>
-                <RemainingBudget />
-              </div>
-            </div>
-            <div className='row'>
-              <div className='col-lg-12'>
-                <AddExpenseForm />
-              </div>
-            </div>
-          </div>
-            
-          <div className='col-lg-8'>
-          <ExpenseList />
-          </div>
-				</div>
-
-       </div>
-
-			</div>
+      </main>    
 		</AppProvider>
-
+    </>
 	);
 };
 
